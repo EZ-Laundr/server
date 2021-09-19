614a7c2944612dfbe0ade2914adb3009c848a278
Garry <Garry@Ivys-MacBook-2.local>
2021-09-19 15:37:28 +0700

    tdd-errorHandler

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,215 +1,217 @@
 const { signToken } = require("../helpers/jwt");
 const { checkPassword } = require("../helpers/bcrypt");
 const {
-	User,
-	Perfume,
-	Service,
-	SpecialTreatment,
-	Order,
-	OrderSpecial,
+    User,
+    Perfume,
+    Service,
+    SpecialTreatment,
+    Order,
+    OrderSpecial,
 } = require("../models");
 
 class Controller {
-	static async register(req, res, next) {
-		try {
-			const { email, password, phoneNumber } = req.body;
-			const payload = {
-				email,
-				password,
-				phoneNumber,
-				role: "customer",
-			};
+    static async register(req, res, next) {
+        try {
+            const { email, password, phoneNumber } = req.body;
+            const payload = {
+                email,
+                password,
+                phoneNumber,
+                role: "customer",
+            };
 
-			const result = await User.create(payload);
-			const access_token = signToken({
-				id: result.id,
-				email,
-				phoneNumber,
-				role: result.role,
-			});
-			res.status(201).json({
-				email,
-				role: result.role,
-				access_token,
-			});
-		} catch (err) {
-			next(err);
-		}
-	}
+            const result = await User.create(payload);
+            const access_token = signToken({
+                id: result.id,
+                email,
+                phoneNumber,
+                role: result.role,
+            });
+            res.status(201).json({
+                email,
+                role: result.role,
+                access_token,
+            });
+        } catch (err) {
+            next(err);
+        }
+    }
 
-	static async login(req, res, next) {
-		try {
-			const { email, password } = req.body;
+    static async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
 
-			const result = await User.findOne({
-				where: {
-					email,
-					role: "customer",
-				},
-			});
-			if (!result) {
-				throw {
-					name: "Unauthorized",
-					msg: "invalid username or email password",
-				};
-			}
+            const result = await User.findOne({
+                where: {
+                    email,
+                    role: "customer",
+                },
+            });
+            if (!result) {
+                throw {
+                    name: "Unauthorized",
+                    msg: "invalid username or email password",
+                };
+            }
 
-			const { id, phoneNumber, role } = result;
+            const { id, phoneNumber, role } = result;
 
-			if (!checkPassword(password, result.password)) {
-				throw {
-					name: "Unauthorized",
-					msg: "invalid username or email password",
-				};
-			}
+            if (!checkPassword(password, result.password)) {
+                throw {
+                    name: "Unauthorized",
+                    msg: "invalid username or email password",
+                };
+            }
 
-			const access_token = signToken({
-				id,
-				email,
-				phoneNumber,
-				role,
-			});
+            const access_token = signToken({
+                id,
+                email,
+                phoneNumber,
+                role,
+            });
 
-			res.status(200).json({
-				email,
-				role,
-				access_token,
-			});
-		} catch (err) {
-			next(err);
-		}
-	}
+            res.status(200).json({
+                email,
+                role,
+                access_token,
+            });
+        } catch (err) {
+            next(err);
+        }
+    }
 
-	static async getPerfumes(req, res, next) {
-		try {
-			const result = await Perfume.findAll();
+    static async getPerfumes(req, res, next) {
+        try {
+            const result = await Perfume.findAll();
 
-			res.status(200).json(result);
-		} catch (err) {
-			next(err);
-		}
-	}
+            res.status(200).json(result);
+        } catch (err) {
+            next(err);
+        }
+    }
 
-	static async getServices(req, res, next) {
-		try {
-			const result = await Service.findAll();
+    static async getServices(req, res, next) {
+        try {
+            const result = await Service.findAll();
 
-			res.status(200).json(result);
-		} catch (err) {
-			next(err);
-		}
-	}
+            res.status(200).json(result);
+        } catch (err) {
+            next(err);
+        }
+    }
 
-	static async getSpecialTreatments(req, res, next) {
-		try {
-			const result = await SpecialTreatment.findAll();
+    static async getSpecialTreatments(req, res, next) {
+        try {
+            const result = await SpecialTreatment.findAll();
 
-			res.status(200).json(result);
-		} catch (err) {
-			next(err);
-		}
-	}
+            res.status(200).json(result);
+        } catch (err) {
+            next(err);
+        }
+    }
 
-	static async getOrders(req, res, next) {
-		try {
-			const { id: UserId } = req.user;
-			const result = await Order.findAll({
-				where: {
-					UserId,
-				},
-				include: [{ model: OrderSpecial, include: [SpecialTreatment] }],
-				order: [["updatedAt", "DESC"]],
-			});
+    static async getOrders(req, res, next) {
+        try {
+            const { id: UserId } = req.user;
+            const result = await Order.findAll({
+                where: {
+                    UserId,
+                },
+                include: [{ model: OrderSpecial, include: [SpecialTreatment] }],
+                order: [["updatedAt", "DESC"]],
+            });
 
-			res.status(200).json(result);
-		} catch (err) {
-			next(err);
-		}
-	}
+            res.status(200).json(result);
+        } catch (err) {
+            next(err);
+        }
+    }
 
-	static async getOrdersById(req, res, next) {
-		try {
-			const { id: UserId } = req.user;
-			const id = req.params.id;
+    static async getOrdersById(req, res, next) {
+        try {
+            const { id: UserId } = req.user;
+            const id = req.params.id;
 
-			const result = await Order.findOne({
-				where: {
-					id,
-					UserId,
-				},
-				include: [
-					{ model: OrderSpecial, include: [SpecialTreatment] },
-					Service,
-				],
-			});
+            const result = await Order.findOne({
+                where: {
+                    id,
+                    UserId,
+                },
+                include: [
+                    { model: OrderSpecial, include: [SpecialTreatment] },
+                    Service,
+                ],
+            });
 
-			res.status(200).json(result);
-		} catch (err) {
-			next(err);
-		}
-	}
+            res.status(200).json(result);
+        } catch (err) {
+            next(err);
+        }
+    }
 
-	static async postOrders(req, res, next) {
-		try {
-			const { id: UserId } = req.user;
-			let {
-				pickup,
-				ServiceId,
-				perfume,
-				treatments,
-				customerAddress,
-				rangeAddress,
-			} = req.body;
+    static async postOrders(req, res, next) {
+        try {
+            const { id: UserId } = req.user;
+            let {
+                pickup,
+                ServiceId,
+                perfume,
+                treatments,
+                customerAddress,
+                rangeAddress,
+            } = req.body;
 
-			const payload = {
-				weight: 0,
-				status: "pending",
-				totalPrice: 0,
-				pickup,
-				UserId,
-				ServiceId,
-				customerAddress: customerAddress || "",
-				rangeAddress: rangeAddress || 0,
-				PerfumeId: perfume.id,
-			};
-			const result = await Order.create(payload, { include: ["Perfume"] });
+            const payload = {
+                weight: 0,
+                status: "pending",
+                totalPrice: 0,
+                pickup,
+                UserId,
+                ServiceId,
+                customerAddress: customerAddress || "",
+                rangeAddress: rangeAddress || 0,
+                PerfumeId: perfume.id,
+            };
+            const result = await Order.create(payload, {
+                include: ["Perfume"],
+            });
 
-			const { id } = result;
+            const { id } = result;
 
-			const specialPayload = treatments.map((treatment) => {
-				return {
-					SpecialTreatmentId: treatment.id,
-					quantity: treatment.qty,
-					OrderId: id,
-					price: treatment.qty * treatment.price,
-				};
-			});
+            const specialPayload = treatments.map((treatment) => {
+                return {
+                    SpecialTreatmentId: treatment.id,
+                    quantity: treatment.qty,
+                    OrderId: id,
+                    price: treatment.qty * treatment.price,
+                };
+            });
 
-			const result2 = await OrderSpecial.bulkCreate(specialPayload, {
-				returning: true,
-				include: [Order],
-			});
+            const result2 = await OrderSpecial.bulkCreate(specialPayload, {
+                returning: true,
+                include: [Order],
+            });
 
-			const newPrice = result2.map((e) => e.price);
-			const totalPrice = newPrice.reduce((a, b) => a + b);
+            const newPrice = result2.map((e) => e.price);
+            const totalPrice = newPrice.reduce((a, b) => a + b);
 
-			const result3 = await Order.update(
-				{
-					totalPrice: totalPrice + perfume.price,
-				},
-				{
-					where: {
-						id,
-					},
-					returning: true,
-				}
-			);
+            const result3 = await Order.update(
+                {
+                    totalPrice: totalPrice + perfume.price,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                    returning: true,
+                }
+            );
 
-			res.status(200).json(result3[1][0]);
-		} catch (err) {
-			next(err);
-		}
-	}
+            res.status(200).json(result3[1][0]);
+        } catch (err) {
+            next(err);
+        }
+    }
 }
 
 module.exports = Controller;
--- tests/getPerfumes.test.js
+++ tests/getPerfumes.test.js
@@ -1,138 +1,226 @@
 const app = require("../app");
 const request = require("supertest");
 const { User, Perfume } = require("../models");
-const { dummyUser } = require("./dummy-user.json");
+const { dummyUser, dummyUser2 } = require("./dummy-user.json");
 const { signToken } = require("../helpers/jwt");
 let access_token;
+let accessTokenUser;
 let testedId;
 
 beforeAll((done) => {
-	User.create({
-		phoneNumber: dummyUser.phoneNumber,
-		email: dummyUser.email,
-		password: dummyUser.password,
-		role: "admin",
-	})
-		.then((result) => {
-			access_token = signToken({
-				id: result.id,
-				email: result.email,
-				phoneNumber: result.phoneNumber,
-				role: result.role,
-			});
-			done();
-		})
-		.catch((err) => done(err));
+    User.create({
+        phoneNumber: dummyUser.phoneNumber,
+        email: dummyUser.email,
+        password: dummyUser.password,
+        role: "admin",
+    })
+        .then((result) => {
+            access_token = signToken({
+                id: result.id,
+                email: result.email,
+                phoneNumber: result.phoneNumber,
+                role: result.role,
+            });
+            done();
+        })
+        .catch((err) => done(err));
+    User.create({
+        phoneNumber: dummyUser2.phoneNumber,
+        email: dummyUser2.email,
+        password: dummyUser2.password,
+        role: "customer",
+    })
+        .then((result) => {
+            accessTokenUser = signToken({
+                id: result.id,
+                email: result.email,
+                phoneNumber: result.phoneNumber,
+                role: result.role,
+            });
+            done();
+        })
+        .catch((err) => done(err));
 });
 
 afterAll((done) => {
-	User.destroy({
-		truncate: true,
-		cascade: true,
-		restartIdentity: true,
-	})
-		.then(() => done())
-		.catch((err) => done(err));
+    User.destroy({
+        truncate: true,
+        cascade: true,
+        restartIdentity: true,
+    })
+        .then(() => done())
+        .catch((err) => done(err));
 });
 
 describe("GET /perfumes", () => {
-	test("success", (done) => {
-		request(app)
-			.get("/perfumes")
-			.then((response) => {
-				expect(response.status).toBe(200);
-				expect(response.body[0]).toHaveProperty("id");
-				expect(response.body[0]).toHaveProperty("name");
-				expect(response.body[0]).toHaveProperty("price");
-				expect(response.body[0]).toHaveProperty("imageUrl");
-				done();
-			})
-			.catch((err) => {
-				done(err);
-			});
-	});
+    test("success", (done) => {
+        request(app)
+            .get("/perfumes")
+            .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body[0]).toHaveProperty("id");
+                expect(response.body[0]).toHaveProperty("name");
+                expect(response.body[0]).toHaveProperty("price");
+                expect(response.body[0]).toHaveProperty("imageUrl");
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
 });
 
 describe("GET /admin/perfumes", () => {
-	test("success", (done) => {
-		request(app)
-			.get("/admin/perfumes")
-			.set("access_token", access_token)
-			.then((response) => {
-				expect(response.status).toBe(200);
-				expect(response.body[0]).toHaveProperty("id");
-				expect(response.body[0]).toHaveProperty("name");
-				expect(response.body[0]).toHaveProperty("price");
-				expect(response.body[0]).toHaveProperty("imageUrl");
-				done();
-			})
-			.catch((err) => {
-				done(err);
-			});
-	});
+    test("success", (done) => {
+        request(app)
+            .get("/admin/perfumes")
+            .set("access_token", access_token)
+            .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body[0]).toHaveProperty("id");
+                expect(response.body[0]).toHaveProperty("name");
+                expect(response.body[0]).toHaveProperty("price");
+                expect(response.body[0]).toHaveProperty("imageUrl");
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+});
+
+describe("GET /admin/perfumes", () => {
+    test("invalid token", (done) => {
+        request(app)
+            .get("/admin/perfumes")
+            .set("access_token", "wrongaccesstoken")
+            .then((response) => {
+                expect(response.status).toBe(401);
+                expect(response.body).toHaveProperty("msg");
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+});
+
+describe("GET /admin/perfumes/:id", () => {
+    test("not found", (done) => {
+        request(app)
+            .get(`/admin/perfumes/${testedId + 1}`)
+            .set("access_token", access_token)
+            .then((response) => {
+                expect(response.status).toBe(404);
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
 });
 
 describe("POST admin/perfumes", () => {
-	test("success", (done) => {
-		request(app)
-			.post("/admin/perfumes")
-			.set("Accept", "application/json")
-			.set("access_token", access_token)
-			.send({
-				name: "perfumes baru",
-				imageUrl: "image",
-				price: 2000,
-			})
-			.then((response) => {
-				expect(response.status).toBe(201);
-				expect(response.body).toHaveProperty("id");
-				expect(response.body).toHaveProperty("price");
-				expect(response.body).toHaveProperty("imageUrl");
-				testedId = response.body.id;
-				done();
-			})
-			.catch((err) => {
-				done(err);
-			});
-	});
+    test("success", (done) => {
+        request(app)
+            .post("/admin/perfumes")
+            .set("Accept", "application/json")
+            .set("access_token", access_token)
+            .send({
+                name: "perfumes baru",
+                imageUrl: "image",
+                price: 2000,
+            })
+            .then((response) => {
+                expect(response.status).toBe(201);
+                expect(response.body).toHaveProperty("id");
+                expect(response.body).toHaveProperty("price");
+                expect(response.body).toHaveProperty("imageUrl");
+                testedId = response.body.id;
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+});
+
+describe("POST admin/perfumes", () => {
+    test("Unauthorized", (done) => {
+        request(app)
+            .post("/admin/perfumes")
+            .set("Accept", "application/json")
+            .set("access_token", accessTokenUser)
+            .send({
+                name: "perfumes baru",
+                imageUrl: "image",
+                price: 2000,
+            })
+            .then((response) => {
+                expect(response.status).toBe(403);
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
 });
 
 describe("PUT admin/perfumes", () => {
-	test("success", (done) => {
-		request(app)
-			.put(`/admin/perfumes/${testedId}`)
-			.set("Accept", "application/json")
-			.set("access_token", access_token)
-			.send({
-				name: "EDITED SAKURA",
-			})
-			.then((response) => {
-				expect(response.status).toBe(200);
-				expect(response.body).toHaveProperty("id");
-				expect(response.body).toHaveProperty("name");
-				expect(response.body).toHaveProperty("price");
-				expect(response.body).toHaveProperty("imageUrl");
-				done();
-			})
-			.catch((err) => {
-				done(err);
-			});
-	});
+    test("success", (done) => {
+        request(app)
+            .put(`/admin/perfumes/${testedId}`)
+            .set("Accept", "application/json")
+            .set("access_token", access_token)
+            .send({
+                name: "EDITED SAKURA",
+            })
+            .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body).toHaveProperty("id");
+                expect(response.body).toHaveProperty("name");
+                expect(response.body).toHaveProperty("price");
+                expect(response.body).toHaveProperty("imageUrl");
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
+});
+
+describe("PUT admin/perfumes", () => {
+    test("wrong input type", (done) => {
+        request(app)
+            .put(`/admin/perfumes/${testedId}`)
+            .set("Accept", "application/json")
+            .set("access_token", access_token)
+            .send({
+                price: "twothousand",
+            })
+            .then((response) => {
+                expect(response.status).toBe(500);
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
 });
 
 describe("DELETE admin/perfumes", () => {
-	test("success", (done) => {
-		request(app)
-			.delete(`/admin/perfumes/${testedId}`)
-			.set("Accept", "application/json")
-			.set("access_token", access_token)
-			.then((response) => {
-				expect(response.status).toBe(201);
-				expect(response.body).toHaveProperty("msg");
-				done();
-			})
-			.catch((err) => {
-				done(err);
-			});
-	});
+    test("success", (done) => {
+        request(app)
+            .delete(`/admin/perfumes/${testedId}`)
+            .set("Accept", "application/json")
+            .set("access_token", access_token)
+            .then((response) => {
+                expect(response.status).toBe(201);
+                expect(response.body).toHaveProperty("msg");
+                done();
+            })
+            .catch((err) => {
+                done(err);
+            });
+    });
 });
